c7d4d7990905eb20d6c48918f56a4bd0df7b61cd
Foot Lettsus <92236470+LynxYT101@users.noreply.github.com>
2023-10-08 16:12:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function startClassification(){
+
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/BwVqtt99p/ ',modelReady);
+    
+}
+
+function modelReady(){
+
+    classifier.classify(gotResults); 
+ 
+ }
+ 
+ function gotResults(error,results){
+ 
+     if(error){
+ 
+         console.error(error);
+ 
+     }
+     else{
+ 
+         console.log(results);
+ 
+     }
+ 
+ }
